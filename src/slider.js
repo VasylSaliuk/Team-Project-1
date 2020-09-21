5138dfd844b7ac2b91a95a50536cc79a15258f4c
Vasyl Saliuk <vace1986@gmail.com>
2020-09-21 12:58:23 +0300

    try fix slider

--- src/slider.js
+++ src/slider.js
@@ -1,23 +1,26 @@
 $(document).ready(function () {
     $('.slider').slick({
-        arrows:false,
-        dots:false,
-        adaptiveHeight:false,
+        arrows:true,
+        dots:true,
+        adaptiveHeight:true,
         slidesToShow: 7,
         slidesToScroll: 1,
         infinite:true,
         speed:1000,
-        autoplay:true,
-        autoplaySpeed:10000,
+        autoplay:false,
+        autoplaySpeed: 10000,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        pauseOnDotsHover: true,
+        swipe:true,
         waitForAnimate:true,
         variableWidth: false,
         centerMode:false,
         asNavFor:".sliderbig"
     });
-    
     $('.sliderbig').slick({
-        arrows:true,
-        fade:true,
+        arrows: false,
         asNavFor:".slider"
-    });    
+    });
+     
 });
