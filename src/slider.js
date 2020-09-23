3a38f5238a19fb9d947c77ba21f6b3efff649dca
Vasyl Saliuk <vace1986@gmail.com>
2020-09-23 20:45:08 +0300

    Update slider.js

--- src/slider.js
+++ src/slider.js
@@ -1,36 +1,35 @@
 $(document).ready(function () {
-    $('.slider').slick({
-        arrows:false,
-        dots:true,
-        adaptiveHeight:false,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        infinite:true,
-        speed:1000,
-        autoplay:false,
-        autoplaySpeed: 10000,
-        pauseOnFocus: true,
-        pauseOnHover: true,
-        pauseOnDotsHover: true,
-        swipe:true,
-        waitForAnimate:true,
-        variableWidth: false,
-        centerMode:true,
-        asNavFor: ".sliderbig",
-        responsive: [
-            {
-                breakpoint:767,
-                    settings: {
-                    slidesToShow:2
-                }
-            }
-        ],
-        mobileFirst:false
-    });
-    $('.sliderbig').slick({
-        arrows: true,
-        fade:true,
-        asNavFor:".slider"
-    });
-     
+  $('.slider').slick({
+    arrows: false,
+    dots: true,
+    adaptiveHeight: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    infinite: true,
+    speed: 1000,
+    autoplay: false,
+    autoplaySpeed: 10000,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+    swipe: true,
+    waitForAnimate: true,
+    variableWidth: false,
+    centerMode: true,
+    asNavFor: '.sliderbig',
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+    mobileFirst: false,
+  });
+  $('.sliderbig').slick({
+    arrows: true,
+    fade: true,
+    asNavFor: '.slider',
+  });
 });
